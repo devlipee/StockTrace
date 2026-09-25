49b0bd2244d74000a2ce73722f35a2f22e28a1bd
Luiz Felipe <sluizfelipe692@gmail.com>
2026-09-24 23:14:14 -0300

    feat: implenta contÃeúdo em LojaService

--- src/main/java/org/stocktrace/service/LojaService.java
+++ src/main/java/org/stocktrace/service/LojaService.java
@@ -1,4 +1,41 @@
 package org.stocktrace.service;
 
+import org.stocktrace.model.Loja;
+import org.stocktrace.repository.LojaRepository;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class LojaService {
+
+    private final LojaRepository lojaRepository = new LojaRepository();
+
+    public Loja cadastrarLoja(
+            String nome,
+            String cidade,
+            String bairro,
+            String rua,
+            String numero,
+            String complemento,
+            String cep
+    ) throws SQLException, IOException {
+
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException(
+                    "O nome da loja é obrigatório."
+            );
+        }
+
+        Loja loja = new Loja(
+                nome,
+                cidade,
+                bairro,
+                rua,
+                numero,
+                complemento,
+                cep
+        );
+
+        return lojaRepository.cadastrarLoja(loja);
+    }
 }
